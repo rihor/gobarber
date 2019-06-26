001ade12fdf9c52889b08dfca3392fd2618de816
Pedro Pinho <rihor1000@gmail.com>
2019-06-26 14:22:38 -0300

    Atualizando User.js adicionanda encriptação da senha hook para encriptar senha antes de salvar método para checagem de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,14 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
+// não precisa ser exatamente como a tabela da banco de dados
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // virtual é um campo que não existe no bd
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +16,19 @@ class User extends Model {
         sequelize,
       }
     );
+    // fazer o hash da senha
+    this.addHook('beforeSave', async user => {
+      // para ser chamado apenas caso esteja informando uma senha
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
