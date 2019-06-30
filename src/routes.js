cf24de52beaf59727cea4e55f625bb6d1275c8e0
Pedro Pinho <rihor1000@gmail.com>
2019-06-30 13:04:09 -0300

    Rota de criação de agendamentos feita

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/appointments', AppointmentController.store);
 
 // rota de upload de imagem
 routes.post('/files', upload.single('file'), FileController.store);
