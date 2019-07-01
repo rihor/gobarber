7af185e4663d05c00c26604c439eb46575ef3d98
Pedro Pinho <rihor1000@gmail.com>
2019-07-01 12:26:58 -0300

    Criado controlador de notificações Metodo index feito

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,26 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    // checa se é um provedor
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only a provider can load notifications' });
+    }
+
+    // pegar todas as notificações do provedor
+    const notifications = await Notification.find({ user: req.userId })
+      .sort({ createAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+}
+
+export default new NotificationController();
