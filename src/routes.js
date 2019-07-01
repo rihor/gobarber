6c0f0474d5ff62642bfcb688b65e38e9eac1cd83
Pedro Pinho <rihor1000@gmail.com>
2019-07-01 16:02:31 -0300

    Criado cancelamento de agendamento rota '/appointments/:id' usando o método delete do AppointmentController

--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,7 @@ routes.put('/users', UserController.update);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
