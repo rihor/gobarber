4a5dc81dbab5e1997fb003ae1e4100eebfcdbcd2
Pedro Pinho <rihor1000@gmail.com>
2019-07-02 18:26:41 -0300

    AppointmentController atualizado Usando Queue para definir job de envio de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,9 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -156,18 +158,12 @@ class AppointmentController {
 
     await appointment.save();
 
-    // enviar email
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: `Agendamento cancelado`,
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    /** o objeto passado é recuperado dentro de CancellationMail
+     * dentro da variavel data
+     *  */
+
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
