7fdd3544cdd9a70aadb4dff70c38e28485f1ed69
Pedro Pinho <rihor1000@gmail.com>
2019-07-01 21:39:35 -0300

    Enviando email para o provedor Quando alguém desmarcar o horário com o provedor Mudada a busca feita pelos agendamentos, agora puxa dados do provedor

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -120,7 +121,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     // caso o usuário esteja deletando um agendamento que não é dele
     if (appointment.user_id !== req.userId) {
@@ -141,6 +150,13 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    // enviar email
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: `Agendamento cancelado`,
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
