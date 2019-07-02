fb3297e02ade06f7c885f8f388cdb8bcbc2a5488
Pedro Pinho <rihor1000@gmail.com>
2019-07-02 18:25:49 -0300

    Criada configuração de file do Redis

---
+++ src/lib/Queue.js
@@ -0,0 +1,41 @@
+/**
+ * Para cada job é criada uma fila e dentro dessa fila
+ * é armazenada o 'bee' que é a instancia que conecta ao Redis
+ * e também o método que processa o job
+ */
+import Bee from 'bee-queue';
+
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, { redis: redisConfig }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+export default new Queue();
