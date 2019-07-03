90155c744ee237ce9c8cd40aff1dc5f3420e8e1a
Pedro Pinho <rihor1000@gmail.com>
2019-07-03 15:15:14 -0300

    Criada rota de listagem de horários disponíveis

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -33,5 +34,6 @@ routes.put('/notifications/:id', NotificationController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 export default routes;
