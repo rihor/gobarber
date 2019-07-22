e1a5dde72642d335ce49d0ace9608ec5c2f70f4b
Pedro Pinho <rihor1000@gmail.com>
2019-07-22 17:21:18 -0300

    Adicionado PersistGate Manter usuário logado mesmo depois de dar refresh na aplicação

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // iniciar o Reactotron, precisa ser iniciado antes da store
 import './config/ReactotronConfig';
@@ -9,16 +10,18 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 // gerenciador de historico de seção
 import history from './services/history';
-import store from './store';
+import { store, persistor } from './store';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
