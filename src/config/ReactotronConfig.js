1a4dc0bea16ecd270d78910ee1e482b1629d1253
Pedro Pinho <rihor1000@gmail.com>
2019-07-31 17:45:05 -0300

    Adicionando configuração do Reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,16 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+if (__DEV__) {
+  // precisa colocar o ip do computador se estiver usando android
+  const tron = Reactotron.configure({ host: '192.168.100.8' })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
