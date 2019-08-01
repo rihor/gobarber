313a61baaab8b2aea563f7b6f0cd5837f69142ec
Pedro Pinho <rihor1000@gmail.com>
2019-08-01 14:04:49 -0300

    Adicionando o ip dentro da api Fiz isso para poder usar o ip na hora de editar a url da imagem O banco retorna a imagem como se eu estivesse no localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+export const ip = `192.168.100.8`;
+
 const api = axios.create({
-  baseURL: 'http://192.168.100.8:3333',
+  baseURL: `http://${ip}:3333`,
 });
 
 export default api;
