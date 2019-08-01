3594d41bab44a202b413c829ccd5841d3d3fde8e
Pedro Pinho <rihor1000@gmail.com>
2019-08-01 15:00:51 -0300

    Adicionada página de Seleção de Provedor

---
+++ src/pages/New/SelectProvider/index.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api, { ip } from '~/services/api';
+import Background from '~/components/Background';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+export default function SelectProvider({ navigation }) {
+  const [providers, serProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      data = response.data.map(provider => ({
+        ...provider,
+        avatar: {
+          ...provider.avatar,
+          url: provider.avatar.url.replace(/localhost/, ip),
+        },
+      }));
+
+      serProviders(data);
+    }
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
+}
+
+SelectProvider.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o prestador',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Dashboard');
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
